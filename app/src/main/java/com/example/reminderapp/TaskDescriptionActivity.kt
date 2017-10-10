f262323e0a67745fafdd189020c8c2c582500630
EthanShiloh <et.shiloh@gmail.com>
2017-10-10 19:14:59 +0300

    Added TimePicker and DatePicker
    They don't do anything, but they will :)

--- app/src/main/java/com/example/reminderapp/TaskDescriptionActivity.kt
+++ app/src/main/java/com/example/reminderapp/TaskDescriptionActivity.kt
@@ -1,11 +1,21 @@
 package com.example.reminderapp
 
 import android.app.Activity
+import android.app.Dialog
+import android.app.DialogFragment
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_task_description.*
+import android.widget.TimePicker
+import android.text.format.DateFormat.is24HourFormat
+import android.app.TimePickerDialog
+import android.text.format.DateFormat
+import java.util.*
+import android.widget.ScrollView
+
+
 
 class TaskDescriptionActivity : AppCompatActivity() {
 
@@ -18,6 +28,8 @@ class TaskDescriptionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task_description)
+
+        timePicker.setIs24HourView(true)
     }
 
     // 3
